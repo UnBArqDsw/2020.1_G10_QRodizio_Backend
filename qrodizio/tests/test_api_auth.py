d5bafb2af5ec412ece67920d2ac3e0a2afa2d061
Fabio Teixeira <fabio1079@gmail.com>
2020-10-10 12:54:38 -0300

    Fix broken tests and add test for role permission

--- qrodizio/tests/test_api_auth.py
+++ qrodizio/tests/test_api_auth.py
@@ -1,17 +1,63 @@
 import json
 
+from .util import get_user_token
+
 
 def test_register_new_employee(client):
     """Test create new employee"""
     email = "newemployee@email.com"
     name = "newemployee"
     password = "newemployee"
+    user_token = get_user_token(client, "fulano@email.com", "fulano")
 
     response = client.post(
         "/auth/register",
         data=json.dumps({"email": email, "password": password, "name": name}),
         content_type="application/json",
+        headers={"Authorization": f"Bearer {user_token}"},
     )
+
+    assert response.status_code == 201
+
+    employee = response.json["employee"]
+
+    assert employee["name"] == name
+    assert employee["email"] == email
+    assert "password" not in employee.keys()
+
+
+def test_only_managers_can_register_new_employees(client):
+    """Test if only users with manager role can create new employees"""
+    email = "other@email.com"
+    name = "other"
+    password = "other"
+
+    def dispatch_request(**headers):
+        return client.post(
+            "/auth/register",
+            data=json.dumps({"email": email, "password": password, "name": name}),
+            content_type="application/json",
+            **headers,
+        )
+
+        return response
+
+    # Act 1: wont allow without authorization
+    response = dispatch_request()
+
+    assert response.status_code == 401
+    assert response.json["error"] == "User unauthorized"
+
+    # Act 2: wont allow a user that don't have manager role
+    user_token = get_user_token(client, "ciclano@email.com", "ciclano")
+    response = dispatch_request(headers={"Authorization": f"Bearer {user_token}"})
+
+    assert response.status_code == 403
+    assert response.json["error"] == "User unauthorized"
+
+    # Act 3: Will allow a user that has manager role
+    user_token = get_user_token(client, "fulano@email.com", "fulano")
+    response = dispatch_request(headers={"Authorization": f"Bearer {user_token}"})
 
     assert response.status_code == 201
 

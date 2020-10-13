fb7fe8d351e29b3e59bd787e8618fee0721f441b
Joao Pedro Gomes <joaok8@gmail.com>
2020-10-13 19:04:02 -0300

    Adicionando link no qrcode

--- qrodizio/views/api/qrcode.py
+++ qrodizio/views/api/qrcode.py
@@ -14,12 +14,13 @@ qrcode_bp = Blueprint("qrcode", __name__, url_prefix="/qrcode")
 
 @qrcode_bp.route("/", methods=["GET"])
 def get_qrcode():    
-    image = qrcode.make('Some data here')
+    image = qrcode.make('https://google.com.br')
     #img = qrcode.make('Some data here', image_factory=PymagingImage)    
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     some_dict = {}
-    some_dict["imagedata"] = base64.b64encode(buffered.getvalue()).decode("ascii")
+    print(base64.b64encode(buffered.getvalue()))
+    some_dict["imagedata"] = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return jsonify(some_dict), 200
 
 

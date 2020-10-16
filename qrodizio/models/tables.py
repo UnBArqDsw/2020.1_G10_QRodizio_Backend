23bacf9c63ef551792274c05cd052721fcc42e9c
caue96 <cauemateus@hotmail.com>
2020-10-15 23:20:35 -0300

    Adicionando relaçao cliente mesa e rota da mesa funcional
    Co-authored-by: jppgomes <joaok8@gmail.com>
    Co-authored-by: Caiocbeleza <caio.almeidabeleza@gmail.com>

--- qrodizio/models/tables.py
+++ qrodizio/models/tables.py
@@ -1,16 +1,41 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy_imageattach.entity import image_attachment
 from qrodizio.ext.database import db
+#from qrodizio.views.api import qrcode
+
+
+association_costumer_table = db.Table(  # Define an N to N association
+    "Costumer_Table_and_Client_association_teste",
+    db.metadata,
+    db.Column("costumer_table_id", db.Integer, db.ForeignKey("CostumerTable.id")),
+    db.Column("client_id", db.Integer, db.ForeignKey("Client.id")),
+)
 
 
 class CostumerTable(db.Model, SerializerMixin):
-    __tablename__ = "costumer_tables"
+    __tablename__ = "CostumerTable"
 
     id = db.Column(db.Integer, primary_key=True)
-    costumers = db.Column(db.Integer)
+    costumers_quantity = db.Column(db.Integer)
+    #qrcode = image_attachment("QRCodeTable")
+    url = db.Column(db.String(255), unique=True, nullable=False)
+    client = db.relationship("Client", secondary=association_costumer_table, backref="CostumerTable")
 
     def create(self):
         db.session.add(self)
         db.session.commit()
 
         return self
-        
+
+class Client(db.Model, SerializerMixin):
+    __tablename__ = "Client"
+
+    id = db.Column(db.Integer, primary_key=True)
+   
+    costumer_table = db.relationship("CostumerTable", secondary=association_costumer_table, backref="Client")
+    
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+        return self

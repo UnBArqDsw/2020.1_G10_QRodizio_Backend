e0409dc4688e2e161a4e57c96b8c09eb7f3bfee8
jppgomes <joaok8@gmail.com>
2020-10-21 19:23:14 -0300

    Adicionando coluna STATUS, CODE na tabela e metodo POST e DELETE nas mesas.
    Co-authored-by: caue96 <cauemateus@hotmail.com>
    Co-authored-by: Caiocbeleza <caio.almeidabeleza@gmail.com>

--- qrodizio/models/tables.py
+++ qrodizio/models/tables.py
@@ -7,19 +7,22 @@ from qrodizio.ext.database import db
 association_costumer_table = db.Table(  # Define an N to N association
     "Costumer_Table_and_Client_association_teste",
     db.metadata,
-    db.Column("costumer_table_id", db.Integer, db.ForeignKey("CostumerTable.id")),
-    db.Column("client_id", db.Integer, db.ForeignKey("Client.id")),
+    db.Column("costumer_table_id", db.Integer, db.ForeignKey("costumer_table.id")),
+    db.Column("client_id", db.Integer, db.ForeignKey("client.id")),
 )
 
 
 class CostumerTable(db.Model, SerializerMixin):
-    __tablename__ = "CostumerTable"
+    __tablename__ = "costumer_table"
+    serialize_rules = ("-clients",)  # prevent recursion error on to_dict
 
     id = db.Column(db.Integer, primary_key=True)
     costumers_quantity = db.Column(db.Integer)
     #qrcode = image_attachment("QRCodeTable")
     url = db.Column(db.String(255), unique=True, nullable=False)
-    client = db.relationship("Client", secondary=association_costumer_table, backref="CostumerTable")
+    status = db.Column(db.Boolean, default=False)
+    code = db.Column(db.Integer, default=False)
+    clients = db.relationship("Client", secondary=association_costumer_table, backref="costumer_table")
 
     def create(self):
         db.session.add(self)
@@ -28,11 +31,12 @@ class CostumerTable(db.Model, SerializerMixin):
         return self
 
 class Client(db.Model, SerializerMixin):
-    __tablename__ = "Client"
+    __tablename__ = "client"
+    serialize_rules = ("-costumer_tables",)  # prevent recursion error on to_dict
 
     id = db.Column(db.Integer, primary_key=True)
    
-    costumer_table = db.relationship("CostumerTable", secondary=association_costumer_table, backref="Client")
+    costumer_tables = db.relationship("CostumerTable", secondary=association_costumer_table, backref="client")
     
     def create(self):
         db.session.add(self)

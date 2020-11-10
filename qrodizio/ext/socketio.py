a5f37ed8379b5b5f1a528de20dad0ed9d2aad100
Fabio Teixeira <fabio1079@gmail.com>
2020-11-10 19:13:34 -0300

    fix socketio warmings

--- qrodizio/ext/socketio.py
+++ qrodizio/ext/socketio.py
@@ -4,5 +4,6 @@ socketio = SocketIO()
 
 
 def init_app(app):
-    socketio.init_app(app, cors_allowed_origins="*")
-    socketio.run(app)
+    socketio.init_app(
+        app, cors_allowed_origins="*", async_mode="threading", manage_session=False
+    )
